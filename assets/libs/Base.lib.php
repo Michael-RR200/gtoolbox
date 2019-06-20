9096fb7505268094afb1a4e22d7e3bc8fe949a36
MichaelMtz <michaelmtz@mtzdesigns.com>
2019-06-20 12:20:30 -0600

    Add timing logic

--- assets/libs/Base.lib.php
+++ assets/libs/Base.lib.php
@@ -4,31 +4,43 @@
     - logging of all operations to zresults.log
         - normal entry :  YYYY-MM-DD HH:ii:ss (T:execution time) => [action] : $this->logData
         - error entry :   YYYY-MM-DD HH:ii:ss (T:execution time) => [action] ERROR : $this->logData
-    - set default values based on localhost vs live
-    - error_reporting & date_timezone  
-    - display array in table capabilities
+    - set default values based on localhost vs remote server
 */
     
 class Base {
     # logging variables
     private $logFile = 'assets/logs/zresults.log';
-    public  $baseDir = $_SERVER['DOCUMENT_ROOT'];
+    public  $baseDir = '';
     public  $logData = '';          # data to be logged
     public  $logError = '';         # error data to be logged
     public  $logAction = '';        # explanation of request / action being undertaken
     public  $logPriority = 'INFO';
     
     # timing variables
-    private $startTime = microtime();
+    private $startTime = null;
     
     #general variables 
     public  $localhost = false;     # true :  $_SERVER["HTTP_HOST"] == 'localhost'
    
     function __construct(){
-        
+        $this->startTime = microtime();
+        $this->baseDir = $_SERVER['DOCUMENT_ROOT'];
+        sleep(1);
+        echo ($this->getRequestTime());        
     }#fn constructor
    
     function __destruct() {
         
     }#fn destructor
+
+    /*
+     * fn : getRequestTime  : Returns how long current request has taken.
+     */
+    public function getRequestTime(){
+        $temp = explode(' ',$this->startTime);
+        $start = $temp[0] + $temp[1];
+        $now = explode(' ', microtime());
+        $end = $now[0] + $now[1];
+        return round($end - $start,4);
+    }
 }# class Base
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'assets/libs/Base.lib.php';
+
+$aBase = new Base();
